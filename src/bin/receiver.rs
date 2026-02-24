daa972b6ca5698d562efaa0ff6ab4bdb7ef504e2
Tamaraife <tamaradaye0@gmail.com>
2026-02-24 16:30:06 +0100

    multithreading for nonblocking io

--- src/bin/receiver.rs
+++ src/bin/receiver.rs
@@ -2,34 +2,48 @@
 
 use spake2::{Ed25519Group, Identity, Password, Spake2};
 use std::net::{TcpListener, TcpStream};
-use std::io::prelude::*;
+use std::io::{self, prelude::*};
 use std::io::BufReader;
+use std::thread;
+use std::sync::mpsc::{self, Receiver, Sender};
 
 fn main() -> std::io::Result<()> {
+    let (tx, rx) : (Sender<String>, Receiver<String>) = mpsc::channel();
     let mut stream  = TcpStream::connect("127.0.0.1:8000")?;
-    let mut buffer = BufReader::new(stream);
-    let mut message_size = [0; 1];
-    let mut file_name: Vec<u8> = vec![];
-    let mut file_type: Vec<u8> = vec![];
-    buffer.read_exact(&mut message_size)?;
-    let bytes_read: u8 = 0;
-    while bytes_read < message_size[0]{
-        buffer.read_exact
-    }
-    // let mut byte_count: usize = 0;
-    // let mut buf  = [0; 1024];
-    // while byte_count < 11 {
-    //     let bytes_read = stream.read(&mut buf[byte_count..])?;
-    //     if bytes_read == 0 {
-    //         break
-    //     };
-    //     byte_count += bytes_read;
-    // }
-    // println!("{byte_count}: read from the socket");
-    // println!("{:?}", &buf[0..12]);
-    let (s1, outbound_msg) = Spake2::<Ed25519Group>::start_b(
-       &Password::new(b"password"),
-       &Identity::new(b"client id string"),
-       &Identity::new(b"server id string"));
+    let mut write_stream = stream.try_clone()?;
+    let mut read_stream = stream;
+    let mut write_thread  = thread::spawn(move || {
+        loop {
+            let text = rx.recv().unwrap();
+            write_stream.write_all(text.as_bytes()).unwrap();
+        }
+    });
+    let read_thread = thread::spawn(move || {
+        let mut buffer = BufReader::new(read_stream);
+        let mut data = [0; 1024];
+        let mut pointer : usize = 0;
+        loop {
+            let bytes_read = buffer.read(&mut data).unwrap();
+            if bytes_read == 0{
+                break
+            }
+
+            pointer += bytes_read;
+
+            println!("read from tcp socket{:?}", String::from_utf8_lossy(&data[0..pointer]))
+        }
+    });
+    let mut channel_thread = thread::spawn(move || {
+        let stdin = std::io::stdin();
+        loop {
+            let mut input = String::new();
+            stdin.read_line(&mut input).unwrap();
+            tx.send(input).unwrap();
+        }
+    });
+    channel_thread.join().unwrap();
+    write_thread.join().unwrap();
+    read_thread.join().unwrap();
     Ok(())
 }
+
--- src/bin/sender.rs
+++ src/bin/sender.rs
@@ -4,39 +4,60 @@ use std::net::{TcpStream, TcpListener};
 use std::thread;
 use spake2::{Ed25519Group, Identity, Password, Spake2};
 use cli_tool::{generate_hash, Filedata};
+use std::io::BufReader;
+use std::sync::mpsc::{self, Receiver, Sender};
 
 fn main() -> std::io::Result<()> {
+
     let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
     let password: String = generate_hash();
-    println!("{password}");
-    let (s1, outbound_msg) = Spake2::<Ed25519Group>::start_a(
-        &Password::new(b"password"),
-        &Identity::new(b"client id string"),
-        &Identity::new(b"server id string"),
-    );
-
-    let mut file = Filedata{
-        name: String::from("Hello.txt"),
-        file_type: String::from(".text"),
-        file_size: 78
-    };
-
-    let mut data: Vec<u8> = vec![18];
-    data.push(9);
-    data.extend_from_slice(file.name.as_bytes());
-    data.push(5);
-    data.extend_from_slice(file.file_type.as_bytes());
 
      loop {
         match listener.accept() {
             Ok((mut stream, addr)) => {
-                println!("new client: {addr:?}");
-                stream.write_all(password.as_bytes());
-                stream.write_all(&data);
+                let (tx, rx) : (Sender<String>, Receiver<String>) = mpsc::channel();
+                let mut write_stream = stream.try_clone()?;
+                let read_thread = thread::spawn(move || {
+                    let mut buffer = BufReader::new(stream);
+                    let mut pointer : usize = 0;
+                    let mut data = [0; 1024];
+
+                    loop {
+                        let bytes_read = buffer.read(&mut data).unwrap();
+                        if bytes_read == 0 {
+                            break
+                        }
+                        pointer += bytes_read;
+                        println!("Read data from {addr} {:?}", String::from_utf8_lossy(&data[0..pointer]))
+                    }
+                });
+
+                let write_thread = thread::spawn(move || {
+                    loop {
+                        let text = rx.recv().unwrap();
+                        write_stream.write_all(text.as_bytes());
+                    }
+                });
+
+                let channel_thread = thread::spawn(move || {
+                    let stdin = std::io::stdin();
+                    loop {
+                        let mut input= String::new();
+                        stdin.read_line(&mut input).unwrap();
+                        tx.send(input).unwrap();
+                    }
+                });
+
+                channel_thread.join().unwrap();
+                read_thread.join().unwrap();
+                write_thread.join().unwrap();
             },
             Err(e) => println!("couldn't get a client : {e:?}")
         };
     }
+
+    
     Ok(())
 }
+
  
